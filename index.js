8c3e49fa299581ee1a2d04d1062b2d1881e37ef0
Alessio Biancalana <dottorblaster@gmail.com>
2021-07-14 09:38:24 +0200

    Make fastify listen on 0.0.0.0

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const fastify = require('fastify')({
   logger: true,
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 fastify.get('/', ({ query: { payload } }, reply) => {
   reply.send({ hello: payload || 'world' });
 });
 
-fastify.listen(PORT, (err, address) => {
+fastify.listen(PORT, '0.0.0.0', (err, address) => {
   if (err) {
     throw err;
   }
